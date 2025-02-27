c26e04210b9bffedf3375405b71a814398452ef2
harry <harshghavnalkar@gmail.com>
2025-02-27 21:16:28 +0530

    sample project

--- src/demo2.java
+++ src/demo2.java
@@ -1,12 +1,17 @@
 public class demo2{
-
     public static void main(String[] args){
-        int n = 5;
-        for(int i = 1 ; i <= n;i++){
-            System.out.println(i);
+        int num1 = 6;
+        int num2 = 15;
+        int ans;
+        
+        for(int i = 0 ; i < Math.min(num1,num2); i++){
 
+            if(num1 % i == 0 || num2 % i == 0 ){
 
+                ans = i ;
+                System.out.println("your answer is "+ans);
+            }
         }
-
     }
+    
 }
