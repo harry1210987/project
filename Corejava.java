88b26c2a11ad0e3b6904baf549fc3e0edcfd0d91
harry <harshghavnalkar@gmail.com>
2025-03-11 09:50:45 +0530

    recent changes

--- Corejava.java
+++ Corejava.java
@@ -1,10 +1,12 @@
-class Corejava{
+import java.util.Scanner;
 
+public class Corejava{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number ");
+        int num = sc.nextInt();
+        int ans =  num + 22 ; 
+        System.out.println("your answer is "+ans);
 
-        int num = 12 ;
-        
-        System.out.println("your answer is "+num);
-        
     }
 }
